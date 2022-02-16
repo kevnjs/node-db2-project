a45024832ed2c93510b87db96c898454f85d5061
kevnjs <91511588+kevnjs@users.noreply.github.com>
2022-02-16 13:16:30 -0800

    finsihed some

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,9 +1,26 @@
+const cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
   // DO YOUR MAGIC
+  cars.getById(req.params.id).then(car => {
+    if(car.length < 1) return res.status(404).json({message: `car with ${req.params.id} is not found`})
+    next()
+  })
+  
 }
 
 const checkCarPayload = (req, res, next) => {
+  const { body } = req;
+  let missingField;
+  if(!("vin" in body)) missingField = "vin";
+  if(!("make" in body)) missingField = "make";
+  if(!("model" in body)) missingField = "model";
+  if(!("mileage" in body)) missingField = "mileage";
   // DO YOUR MAGIC
+  if(!("vin" in body) || !("make" in body) || !("model") || !("mileage" in body))
+  return res.status(400).json({message: `${missingField} is missing`})
+
+  next()
 }
 
 const checkVinNumberValid = (req, res, next) => {
@@ -12,4 +29,9 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,25 @@
 // DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+const cars = require('./cars-model');
+const mWare = require('./cars-middleware');
+
+router.get('/', (req, res) => {
+    cars.getAll().then(car => res.status(200).json(car))
+    .catch(err => res.status(500).json(err))
+})
+
+router.get('/:id', mWare.checkCarId, (req, res) => {
+    cars.getById(req.params.id).then(car => res.status(200).json(...car))
+    .catch(err => res.status(500).json(err))
+})
+
+router.post('/', mWare.checkCarPayload, (req, res) => {
+    cars.create(req.body).then(car => res.status(201).json(...car))
+})
+
+
+
+
+
+module.exports = router;
